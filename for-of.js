0f9f8809136d03937d7f8b63f7c1edd44bce50ef
Hafsa123456789 <41472581+Hafsa123456789@users.noreply.github.com>
2019-05-22 16:20:02 +0500

    for of loop

---
+++ for-of.js
@@ -0,0 +1,11 @@
+const names=["john","joe","james"]
+for(const name of names)
+{
+    console.log(name)
+}
+//for strings
+const str="this is my place"
+for(const char of str)
+{
+    console.log(char)
+}

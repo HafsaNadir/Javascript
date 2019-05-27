34fad03ea4457cc6d513a060b760506032ccfd21
Hafsa123456789 <41472581+Hafsa123456789@users.noreply.github.com>
2019-05-28 02:20:45 +0500

    comments added

--- set-and-iterators.js
+++ set-and-iterators.js
@@ -6,6 +6,7 @@ console.log(iterator.next()) //{ value: 'February', done: false }
 console.log(iterator.next()) //{ value: 'March', done: false }
 console.log(iterator.next()) //{ value: 'April', done: false } and so on....
 
+//An easier method to loop through the items in a Set is the for...of loop
 for(const iterator of months)
 {
     console.log(iterator)

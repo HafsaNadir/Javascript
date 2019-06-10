07634d8cbd2fca051e05f9fb19ac227c17b752cb
HafsaNadir <hafsanadir21@gmail.com>
2019-06-10 16:02:37 +0500

    more examples

--- destructuring-arrays.js
+++ destructuring-arrays.js
@@ -4,3 +4,15 @@ console.log(firstName) //john
 
 lastName="bran"
 console.log(lastName)
+
+//example
+let array=['a','b','c','d','e']
+const [a,,b]=array
+console.log(a) //a
+console.log(b) //c
+
+//example2
+var[greeting = "hi",name = "Sarah"] = ["hello"]
+
+console.log(greeting)//Hello
+console.log(name)//Sarah

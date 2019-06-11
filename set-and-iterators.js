56af2732a7197232be7d95480589e7221ea2b213
HafsaNadir <hafsanadir21@gmail.com>
2019-06-11 18:24:07 +0500

    accessing values directly

--- set-and-iterators.js
+++ set-and-iterators.js
@@ -1,10 +1,10 @@
 const months = new Set(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
 
 const iterator = months.values()
-console.log(iterator.next()) //{ value: 'January', done: false }
-console.log(iterator.next()) //{ value: 'February', done: false }
-console.log(iterator.next()) //{ value: 'March', done: false }
-console.log(iterator.next()) //{ value: 'April', done: false } and so on....
+console.log(iterator.next().value) //January
+console.log(iterator.next().value) //February
+console.log(iterator.next().value) //March
+console.log(iterator.next().value) //April
 
 //An easier method to loop through the items in a Set is the for...of loop
 for(const iterator of months)

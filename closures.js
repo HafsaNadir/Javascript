0805ce895cfcf87707352a854074b437a4dcdcee
HafsaNadir <hafsanadir21@gmail.com>
2019-06-18 00:09:18 +0500

    more examples

--- closures.js
+++ closures.js
@@ -1,5 +1,5 @@
 function retirement (retirementAge){
- var a="year left until retirement"
+ var a="years left until retirement"
  return function(yearOfBirth)
  {
      var age = 2016 - yearOfBirth
@@ -10,3 +10,17 @@ var r = retirement(66)
 r(1990)
 
 retirement(66)(1990) //same answer as above
+
+//example
+function showName(firstname,lastname)
+{
+    let intro = "name is "
+    return function()
+    {
+        console.log(intro + firstname + " " + lastname)
+    }
+}
+showName('hafsa' , 'nadir')()
+//or
+var name = showName('james' , 'john')
+name()
